f0d46c24caca556a67665468ca1ae7899aed59bb
Saler <satch.lucas@yahoo.com>
2023-06-29 22:05:46 +0200

    Initial commit

---
+++ TilenTask0/EvenNumberPrinter.cs
@@ -0,0 +1,31 @@
+﻿namespace TilenTask0
+{
+    public class EvenNumberPrinter
+    {
+        private int number;
+
+        public void ReadFromConsole()
+        {
+            Console.Write("Enter a positive whole number: ");
+            // If can't be parsed, notify that input is invalid and to try again
+            while (!Int32.TryParse(Console.ReadLine(), out number) || number < 0)
+            {
+                Console.Write("Invalid input, please enter a positive whole number: ");
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Even numbers from 0 to {0} are: ", number);
+            // We should output all even number from 0 to n, but as we all know, 0 is neither negative nor positive
+            // so we have to start with 1 instead 
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ TilenTask0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TilenTask0
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            EvenNumberPrinter printer = new EvenNumberPrinter();
+
+            printer.ReadFromConsole();
+
+            printer.Print();
+        }
+    }
+}
